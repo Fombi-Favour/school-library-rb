0aa44083cc35632f7fffe293fb049c18b92e01cd
Fombi Favour <fombifavour@gmail.com>
2023-11-07 17:00:42 +0100

    Update student.rb

--- student.rb
+++ student.rb
@@ -1,12 +1,12 @@
 class Student < Person
   attr_accessor :classroom
 
-  def initialize (id, name = "unknown", age, parent_permision = true, classroom)
+  def initialize(id, age, parent_permision, classroom, name = 'unknown')
     super(id, name, age, parent_permision)
     @classroom = classroom
   end
 
   def play_hooky
-    "¯\(ツ)/¯"
+    '¯\(ツ)/¯'
   end
 end
