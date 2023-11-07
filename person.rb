c40b687e1a7851cdf946704c0fcbacb3db2c9909
Fombi Favour <fombifavour@gmail.com>
2023-11-07 14:46:37 +0100

    Update person.rb

--- person.rb
+++ person.rb
@@ -1,2 +1,19 @@
 class Person
+  attr_reader :id
+  attr_accessor :name, :age
+  # constructor
+  def initialize (id, name = "unknown", age, parent_permision = true)
+    @id = id
+    @name = name
+    @age = age
+    @parent_permision = parent_permision
+  end
+
+  private def of_age?
+    true if @age >= 18
+  end
+
+  def can_use_services
+    true if of_age? || @parent_permision
+  end
 end
