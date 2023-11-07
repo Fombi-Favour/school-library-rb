d437757664ad5078b2d83a74fce77aeca585b81d
Fombi Favour <fombifavour@gmail.com>
2023-11-07 16:13:28 +0100

    Update student.rb

--- student.rb
+++ student.rb
@@ -1,2 +1,12 @@
-class Student
+class Student < Person
+  attr_accessor :classroom
+
+  def initialize (id, name = "unknown", age, parent_permision = true, classroom)
+    super(id, name, age, parent_permision)
+    @classroom = classroom
+  end
+
+  def play_hooky
+    "¯\(ツ)/¯"
+  end
 end
