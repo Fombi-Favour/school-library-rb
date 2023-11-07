f358451ad5d74fd1dd28d2b0755a7404bf46c1d6
Fombi Favour <fombifavour@gmail.com>
2023-11-07 16:45:19 +0100

    Update teacher.rb

--- teacher.rb
+++ teacher.rb
@@ -1,2 +1,12 @@
-class Teacher
+class Teacher < Person
+  attr_accessor :specialization
+  def initialize (id, name = "unknown", age, parent_permision = true, specialization)
+    super(id, name, age, parent_permision)
+    @specialization = specialization
+  end
+
+  def can_use_services
+    super
+    true
+  end
 end
