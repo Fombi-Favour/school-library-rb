80e348bf1d0ecc51ecad73252e100887a46dec91
Fombi Favour <fombifavour@gmail.com>
2023-11-08 14:45:29 +0100

    Update trimmer_decorator.rb

--- trimmer_decorator.rb
+++ trimmer_decorator.rb
@@ -1,10 +1,5 @@
 class TrimmerDecorator < BaseDecorator
-  def initialize(nameable)
-    super
-    @nameable = nameable
-  end
-
   def correct_name
-    @nameable.correct_name[0..9] if @nameable.correct_name.length > 10
+    @nameable.correct_name[0, 9] if @nameable.correct_name.length >= 10
   end
 end
