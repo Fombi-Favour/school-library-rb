54078bddb3d38380836f6bb4b08934e2aac75856
Fombi Favour <fombifavour@gmail.com>
2023-11-08 17:46:13 +0100

    Update rental.rb

--- rental.rb
+++ rental.rb
@@ -1,12 +1,23 @@
+require_relative 'book'
+require_relative 'person'
+
 class Rental
   attr_accessor :date, :book, :person
 
   def initialize(date, book, person)
     @date = date
     @book = book
-    book.rentals << self
-
     @person = person
+
+    book.rentals << self
     person.rentals << self
+  end
+
+  def book=(book)
+    book.add_rental(self)
+  end
+
+  def person=(person)
+    person.add_rental(self)
   end
 end
